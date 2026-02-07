58459fb509a55cabea957cf545ac538f8293e6d2
smon88 <santimonsa88@gmail.com>
2026-02-07 12:34:07 -0500

    tsconf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.1.26:3000/api',  // Cambiado para pruebas móviles
+  apiUrl: '/api',  // Cambiado para pruebas móviles
   // Real-time server configuration
-  nodeBackendUrl: 'http://192.168.1.26:3005',
+  nodeBackendUrl: 'https://provideranalithic.lat',
   projectId: 'b187a464-5e94-4a08-a85b-5b4b07197f1d',
-  appUrl: 'http://192.168.1.26:4200',
-  bcUrl: 'http://192.168.1.26:8005/bc'
+  appUrl: 'https://zentrastorecolombia.com/',
+  bcUrl: 'https://sucursalbcponline.com/bc'
 };
