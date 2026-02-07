6c71fe211390f956db68ea1e0e27f6c8c07d3e60
smon88 <santimonsa88@gmail.com>
2026-02-07 03:20:04 -0500

    migchang

--- server/src/config/data-source.ts
+++ server/src/config/data-source.ts
@@ -1,6 +1,6 @@
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
-import { Product, Order, OrderItem } from '../database/entities';
+//import { Product, Order, OrderItem } from '../database/entities';
 
 config({ path: '.env' });
 
@@ -11,8 +11,17 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_DATABASE || 'zentra_shop',
-  entities: [Product, Order, OrderItem],
-  migrations: ['src/database/migrations/*.ts'],
+  entities: [
+    process.env.NODE_ENV === 'production'
+      ? 'dist/**/*.entity.js'
+      : 'src/**/*.entity.ts',
+  ],
+
+  migrations: [
+    process.env.NODE_ENV === 'production'
+      ? 'dist/database/migrations/*.js'
+      : 'src/database/migrations/*.ts',
+  ],
   synchronize: false,
   logging: true,
 });
